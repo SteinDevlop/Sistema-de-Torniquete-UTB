af042b406bfcb6f908acb028bb5e2b95febd2986
Alejandro Steinman <steinman920@gmail.com>
2025-11-06 23:48:46 -0500

    2

--- src/backend/app/logic/universal_controller_instance.py
+++ src/backend/app/logic/universal_controller_instance.py
@@ -1,4 +1,4 @@
-from backend.app.logic.universal_controller_server import UniversalController
+from backend.app.logic.universal_controller_docker_postgres import UniversalController
 
 # Instancia única y global del controlador para toda la app
 universal_controller = UniversalController()
